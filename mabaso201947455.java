399939e144467bca9f6922289a5f226635a987fe
magagul <107489738+magagul@users.noreply.github.com>
2022-06-15 11:21:44 +0200

    Delete mabaso201947455.java

--- mabaso201947455.java
+++
@@ -1,8 +0,0 @@
-public class multiply{
-        public static void main(String [] args) [
-             int a=5;
-             int b=2;
-              int c = a * b;
-   System.out.println(" a multiplied by b is equal to " +c);
-}
-}
