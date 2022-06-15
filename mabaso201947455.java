b9b5abcb03a4dc5fcace9eb7de48c72d5fb96f15
magagul <107489738+magagul@users.noreply.github.com>
2022-06-15 11:19:58 +0200

    Add files via upload

---
+++ mabaso201947455.java
@@ -0,0 +1,8 @@
+public class multiply{
+        public static void main(String [] args) [
+             int a=5;
+             int b=2;
+              int c = a * b;
+   System.out.println(" a multiplied by b is equal to " +c);
+}
+}
